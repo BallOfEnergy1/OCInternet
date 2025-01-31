b6d39f5e30a57471eef25cf51c98e7eca6148632
BallOfEnergy <66693744+BallOfEnergy1@users.noreply.github.com>
2025-01-31 18:47:25 +0000

    Some Fixes to TCP, no testing yet.

--- src/lib/IP/protocols/TCP.lua
+++ src/lib/IP/protocols/TCP.lua
@@ -150,21 +150,20 @@ end
 
 -- Assumes there's already a finalization waiting.
 function Session:acceptFinalization()
-  print(1)
   tcp.setup()
   self.status = "CLOSE-WAIT"
+  self.ackNum = self.ackNum + 1
   send(self.targetIP, self.targetPort, {tcp = TCPHeader:new(0x01, self.ackNum, self.seqNum):build(), data = nil})
-  print(2)
   local limit = 60
   
   for _ = 1, limit do
     if(not self.buffer:checkHasData()) then
       send(self.targetIP, self.targetPort, {tcp = TCPHeader:new(0x08, self.ackNum, self.seqNum):build(), data = nil})
+      self.seqNum = self.seqNum + 1
       break
     end
     os.sleep(0.25)
   end
-  print(3)
   
   self.status = "LAST-ACK"
   local message, code = multiport.requestMessageWithTimeout(nil, false, false, 5, 5, function(_, _, _, targetPort, _, receivedMessage) return targetPort == self.targetPort and serialization.unserialize(receivedMessage).protocol == tcpProtocol end, true)
@@ -172,7 +171,6 @@ function Session:acceptFinalization()
     self:reset()
     return nil, code
   end
-  print(4)
   if(message == nil) then
     self:reset()
     return false, "Connection closed; timed out."
@@ -183,7 +181,6 @@ function Session:acceptFinalization()
     self:reset()
     return
   end
-  print(5)
   self.status = "CLOSE"
   self.buffer:clear()
   event.cancel(self.listenerID)
@@ -258,7 +255,6 @@ function Session:reset(sentByOther)
 end
 
 function Session:stop()
-  print(1)
   self.status = "FIN-WAIT-1"
   local message, code = multiport.requestMessageWithTimeout(Packet:new(nil,
     tcpProtocol,
@@ -266,7 +262,6 @@ function Session:stop()
     self.targetPort,
     {tcp = TCPHeader:new(0x08, self.ackNum, self.seqNum):build(), data = nil} -- FIN
   ):build(), false, false, 5, 5, function(_, _, _, targetPort, _, receivedMessage) return targetPort == self.targetPort and serialization.unserialize(receivedMessage).protocol == tcpProtocol end)
-  print(2)
   if(message == nil and code == -1) then
     self:reset()
     self.status = "CLOSE"
@@ -277,18 +272,15 @@ function Session:stop()
     self:reset()
     return false, "Connection closed; timed out."
   end
-  print(3)
   local decoded = serialization.unserialize(message)
   local data = decoded.data
   if(data.tcp.flags ~= 0x01 or data.tcp.ackNum ~= self.seqNum + 1) then -- ACK
     self:reset()
     return
   end
-  print(4)
   self.seqNum = self.seqNum + 1
   self.status = "FIN-WAIT-2"
   message, code = multiport.requestMessageWithTimeout(nil, false, false, 5, 5, function(_, _, _, targetPort, _, receivedMessage) return targetPort == self.targetPort and serialization.unserialize(receivedMessage).protocol == tcpProtocol end, true)
-  print(5)
   if(message == nil and code == -1) then
     self:reset()
     self.status = "CLOSE"
@@ -299,14 +291,12 @@ function Session:stop()
     self:reset()
     return false, "Connection closed; timed out."
   end
-  print(6)
   decoded = serialization.unserialize(message)
   data = decoded.data
   if(data.tcp.flags ~= 0x08 or data.tcp.ackNum ~= self.seqNum + 1) then -- FIN
     self:reset()
     return
   end
-  print(7)
   send(self.targetIP, self.targetPort, {tcp = TCPHeader:new(0x1, self.ackNum, self.seqNum):build(), data = nil}, false) --  Send ACK
   self.ackNum = self.ackNum + 1
   self.status = "TIME-WAIT"
@@ -315,7 +305,6 @@ function Session:stop()
     self.buffer:clear()
     event.cancel(self.listenerID)
     _G.TCP.sessions[self.id] = nil
-    print(8)
   end)
 end
 
